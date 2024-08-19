5bf61ae7be21e49c9162a7ffb372f6c8d89042df
Lohit Muralidharan <lohit308@gmail.com>
2024-08-19 12:03:12 -0500

    added starter socket files w boost beast

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,12 +1,38 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
 #include <iostream>
-#include <boost/shared_ptr.hpp>
+#include <string>
+#include <thread>
+
+namespace ip = boost::asio::ip; // namespace with multiple types and functions under it
+using tcp = ip::tcp; // type tcp
 
 using namespace std;
 
-// https://www.youtube.com/watch?v=3elKoMMuJBA
+// SOURCE: https://www.youtube.com/watch?v=3elKoMMuJBA
+// SOURCE: https://www.youtube.com/watch?v=ZSefPfZqxpo&t=1976s
 
 int main() {
-    boost::shared_ptr<int> ptr(new int);
-    *ptr = 10;
-    cout << "pointer :" << *ptr << endl;
+    
+    // Port and Address
+    auto const address = ip::make_address("127.0.0.1");
+    unsigned short const port = 5000;
+
+    // Used to keep track of all of the tasks and makes sure everything gets done
+    boost::asio::io_context ioc{1};
+
+    // The acceptor accepts incoming connections
+    tcp::acceptor acceptor{ioc, {address, port}};
+
+    while (1) {
+        // Instantiate a Socket
+        tcp::socket socket{ioc};
+
+        // Wait for an incoming connection
+        acceptor.accept(socket);
+
+        // A Connection got accepted
+        cout << "socket accepted" << endl;
+    }
+
 }
